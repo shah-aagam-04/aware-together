252a517ade33eefce4c85ddb53fcca7fbe413877
Vikas Thoutam <vikas.thoutam@gmail.com>
2026-07-12 10:27:53 -0500

    chore: make db a singleton

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -4,5 +4,13 @@ import { drizzle } from 'drizzle-orm/better-sqlite3'
 import * as schema from '../db/schema'
 
 const connectionString = process.env.DATABASE_URL!.replace('file:', '')
-const sqlite = new Database(connectionString)
-export const db = drizzle(sqlite, { schema })
+
+const globalForDb = globalThis as unknown as {
+  db: ReturnType<typeof drizzle<typeof schema>> | undefined
+}
+
+export const db = globalForDb.db ?? drizzle(new Database(connectionString), { schema })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForDb.db = db
+}
